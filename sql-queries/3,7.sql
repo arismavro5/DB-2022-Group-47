5f1a0e7277ac2095b9003722e7d27b187f1bc36f
GiorgosKotsompolis <82601509+GiorgosKotsompolis@users.noreply.github.com>
2022-06-04 00:36:46 +0300

    Delete 3,7.sql

--- sql-queries/3,7.sql
+++
@@ -1,4 +0,0 @@
-SELECT p.Amount,p.`Organization_Organization Name`, s.`Full Name`
-FROM project as p, company as c, stem as s
-WHERE p.`Organization_Organization Name` = c.`Organization Name` and s.`ID Stem`=p.`Stem_ID Stem`
-ORDER BY Amount;

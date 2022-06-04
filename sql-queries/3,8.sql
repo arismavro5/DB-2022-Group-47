5f78f526066daeba680f8e187b034613966bdcc5
GiorgosKotsompolis <82601509+GiorgosKotsompolis@users.noreply.github.com>
2022-06-04 17:42:38 +0300

    Delete 3,8.sql

--- sql-queries/3,8.sql
+++
@@ -1,6 +0,0 @@
-select p.`ID Project`, d.`Title`
-from project p 
-left join deliveries d 
-on p.`ID Project` = d.`Project_ID Project`
-group by p.`ID Project`;
--- where p.`ID Project` = d.`Project_ID Project` ; 

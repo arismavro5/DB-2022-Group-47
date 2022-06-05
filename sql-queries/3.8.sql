bcb7245615bd894a4e10fb9ad2f54ce4ac254040
GiorgosKotsompolis <82601509+GiorgosKotsompolis@users.noreply.github.com>
2022-06-05 20:54:04 +0300

    Add files via upload

--- sql-queries/3.8.sql
+++ sql-queries/3.8.sql
@@ -1,4 +1,5 @@
-select `Project_ID Project`,`Full Name`,count(*) 
+select `Full Name`, count(`Researcher_ID Researcher`) 
 from project_has_researcher, researcher
 where `Project_ID Project` not in(select `Project_ID Project` from deliveries) and `Researcher_ID Researcher` = `ID Researcher` 
-having count(*)>=5
+group by `Researcher_ID Researcher`
+having count(`Researcher_ID Researcher`)>=5
